3778cf378668f6b6f9d050d57730f3417d1082d6
taj-u-LW <tajcsebsmrstu@gmail.com>
2024-05-23 17:55:11 +0600

    Collaborate nothing but getting used to

---
+++ app/Models/Teacher.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    use HasFactory;
+    // I meant to do something new but I haven't done it yet
+    // I'll do some real soon.
+}
