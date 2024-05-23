4dc80f109d52b1df555b419e21057fa971667dd6
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2024-05-23 17:14:45 +0600

    update sidebar nav table contents

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,7 +1,7 @@
 <ul class="list-group">
-  <li class="list-group-item active">Cras justo odio</li>
-  <li class="list-group-item">Dapibus ac facilisis in</li>
-  <li class="list-group-item">Morbi leo risus</li>
-  <li class="list-group-item">Porta ac consectetur ac</li>
-  <li class="list-group-item">Vestibulum at eros</li>
+  <li class="list-group-item active">Projects</li>
+  <li class="list-group-item">Maintain</li>
+  <li class="list-group-item">Issues</li>
+  <li class="list-group-item">Products</li>
+  <li class="list-group-item">Volume</li>
 </ul>
